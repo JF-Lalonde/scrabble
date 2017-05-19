7ba362099c66cde7db1d0590fa6af3c90385880f
JF-Lalonde <lalondej88@gmail.com>
2017-05-19 11:46:59 -0600

    changed wording of third test

--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -15,8 +15,9 @@ class ScrabbleTest < Minitest::Test
     assert_equal 4, Scrabble.new.score("f")
   end
 
-  def test_that_nil_returns_score_of_zero
+  def test_that_empty_word_returns_score_of_zero
     assert_equal 0, Scrabble.new.score("")
   end
+
 
 end
